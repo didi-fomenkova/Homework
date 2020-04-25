adc54b86f056850506b35eaa5ccbbee526a4146a
Fomenkova Alex <59022641+didi-fomenkova@users.noreply.github.com>
2020-04-25 23:51:56 +0300

    27

--- Подмножества/Подмножества.cpp
+++ Подмножества/Подмножества.cpp
@@ -7,11 +7,13 @@ void task_5471();
 void task_11256();
 void task_11283();
 void task_13557();
+void task_1();
+void task_2();
 
 int main() {
 
-	
-	task_13557();
+	setlocale(LC_ALL, "Russian");
+	task_2();
 
 	return 0;
 }
@@ -30,7 +32,7 @@ int main() {
 
 void task_5375() {
 	int size, number;
-	long  maxNeg = -1000000001;
+	long long int maxNeg = -1000000001; // long int = int = long; long long int −9 223 372 036 854 775 808 до +9 223 372 036 854 775 807
 	int kolNeg = 0;
 	int numMaxNeg = 0;
 	int numZero = 0;
@@ -148,7 +150,7 @@ void task_11283() {
 	cout << kol << endl;
 }
 
-/* № 13557 ??  не собирается
+/* № 13557 
 Дан набор из N целых положительных чисел. 
 Необходимо выбрать из набора произвольное количество чисел так,
 чтобы их сумма была как можно больше и при этом не делилась на 6.
@@ -165,7 +167,7 @@ void task_13557() {
 	for (int i = 0; i < size; ++i) {
 		cin >> number;
 		sum += number;
-		if (number % 6 =! 0 &&  min > number)
+		if ((number % 6) != 0 &&  min > number)
 			min = number;
 	}
 	if (sum % 6 != 0)
@@ -179,4 +181,55 @@ void task_13557() {
 		kol = 0;
 	}
 	cout << kol << " " << sum << endl;
+}
+
+void task_1() {
+	int size, t;
+	int aSum = 0;
+	int a, b;
+	int min = INT_MAX;
+	cin >> size;
+	cin >> t;
+	cin >> a >> b;
+	if (b + t < a + t)
+		min = b + t;
+	else
+		min = a + t;
+	aSum += a;
+	for (int i = 1; i < size; ++i) {
+		cin >> a >> b;
+		aSum += a;
+		if (min + b < aSum + t)
+			min += b;
+		else
+			min = aSum + t;
+	}
+	cout << min << endl;
+}
+
+void task_2() {
+	int number;
+	int maxH = 0;
+	int pr = 0;
+	int h = 0;
+	int kol = 0;
+	cin >> number;
+	pr = number;
+	while (number  != 0) {
+		if (number > pr) {
+			h += number - pr;
+		}
+		else {
+			if (h > maxH)
+				maxH = h;
+			h = 0;
+		}
+		kol++;
+		pr = number;
+		cin >> number;
+	}
+	if (h > maxH)
+		maxH = h;
+	cout << "Получено чисел " << kol << endl;
+	cout << "Наибольшая высота подъема " << maxH << endl;
 }
